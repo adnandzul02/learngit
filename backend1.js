6553e571bfcde6f00e56216b321798abf838c57a
Adnan <adnandzul02@gmail.com>
2025-09-06 22:02:40 +0700

    test commit dari staging

---
+++ backend1.js
@@ -0,0 +1,28 @@
+// file: index.js
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Middleware biar bisa baca JSON
+app.use(express.json());
+
+// Route root
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+// Route API simpel
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
+// Route POST latihan
+app.post("/api/echo", (req, res) => {
+  const data = req.body;
+  res.json({ you_sent: data });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
